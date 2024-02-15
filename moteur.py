c7ac05f6cc11a644d920a131b4bb48f3c72f3901
pauldesaintalbin <pauldesaintalbin@gmail.com>
2024-02-15 19:49:43 +0100

    cleanup

--- moteur.py
+++ moteur.py
@@ -1,13 +1,7 @@
-import random, numpy, copy
+import numpy, copy
 
 def initialiser_plateau(nb_lignes, nb_colonnes):
     return [[' ' for i in range(nb_colonnes)] for i in range(nb_lignes)]
-
-def afficher_plateau(plateau):
-    for ligne in plateau:
-        
-        print('|'.join(ligne))
-        print('-' * (len(ligne) * 2 - 1))
 
 def coup_valide(plateau, colonne):
     return 0 <= colonne < len(plateau[0]) and plateau[0][colonne] == ' '
@@ -118,119 +112,6 @@ def est_gagnant(plateau, piece, emplacement):
 def plateau_plein(plateau):
     return all(cellule != ' ' for ligne in plateau for cellule in ligne)
 
-def jouer():
-    nb_lignes = 6
-    nb_colonnes = 7
-    plateau = initialiser_plateau(nb_lignes, nb_colonnes)
-    piece_actuelle = 'X'
-
-    mode_jeu = input("Choisissez le mode de jeu (1 pour jouer contre l'ordinateur, 2 pour joueur contre joueur): ")
-
-    if mode_jeu == '1':
-        jouer_contre_ordinateur(plateau, piece_actuelle, nb_colonnes)
-    elif mode_jeu == '2':
-        jouer_joueur_contre_joueur(plateau, piece_actuelle)
-    else:
-        print("Mode de jeu non valide.")
-
-def jouer_joueur_contre_joueur(plateau, piece_actuelle):
-    while True:
-        afficher_plateau(plateau)
-        try:
-            colonne = int(input(f"Joueur {piece_actuelle}, choisissez une colonne (0-{len(plateau[0])-1}): "))
-        except ValueError:
-            print("Veuillez entrer un nombre valide.")
-            continue
-
-        if coup_valide(plateau, colonne):
-            stocker_piece(plateau, colonne, piece_actuelle)
-
-            if est_gagnant(plateau, piece_actuelle, [obtenir_ligne(plateau, colonne) + 1, colonne]):
-                afficher_plateau(plateau)
-                print(f"Joueur {piece_actuelle} a gagné !")
-                break
-            elif plateau_plein(plateau):
-                afficher_plateau(plateau)
-                print("Match nul !")
-                break
-            else:
-                piece_actuelle = 'O' if piece_actuelle == 'X' else 'X'
-        else:
-            print("Colonne invalide. Veuillez choisir à nouveau.")
-
-def jouer_contre_ordinateur_random(plateau, piece_actuelle, nb_colonnes):
-    while True:
-        afficher_plateau(plateau)
-
-        if piece_actuelle == 'X':
-            try:
-                colonne = int(input(f"Joueur {piece_actuelle}, choisissez une colonne (0-{nb_colonnes-1}): "))
-            except ValueError:
-                print("Veuillez entrer un nombre valide.")
-                continue
-            
-            if coup_valide(plateau, colonne):
-                stocker_piece(plateau, colonne, piece_actuelle)
-            else:
-                print("Colonne invalide. Veuillez choisir à nouveau.")
-                continue
-        else:
-            colonne = random.randint(0, nb_colonnes - 1)
-            while not coup_valide(plateau, colonne):
-                colonne = random.randint(0, nb_colonnes - 1)
-            stocker_piece(plateau, colonne, piece_actuelle)
-            print(f"L'ordinateur a choisi la colonne {colonne}.")
-
-        if est_gagnant(plateau, piece_actuelle, [obtenir_ligne(plateau, colonne) + 1, colonne]):
-            afficher_plateau(plateau)
-            if piece_actuelle == 'X':
-                print(f"Joueur {piece_actuelle} a gagné !")
-            else:
-                print("L'ordinateur a gagné !")
-            break
-        elif plateau_plein(plateau):
-            afficher_plateau(plateau)
-            print("Match nul !")
-            break
-        else:
-            piece_actuelle = 'O' if piece_actuelle == 'X' else 'X'
-
-def jouer_contre_ordinateur(plateau, piece_actuelle, nb_colonnes):
-    while True:
-        afficher_plateau(plateau)
-
-        if piece_actuelle == 'X':
-            try:
-                colonne = int(input(f"Joueur {piece_actuelle}, choisissez une colonne (0-{nb_colonnes-1}): "))
-            except ValueError:
-                print("Veuillez entrer un nombre valide.")
-                continue
-            
-            if coup_valide(plateau, colonne):
-                stocker_piece(plateau, colonne, piece_actuelle)
-            else:
-                print("Colonne invalide. Veuillez choisir à nouveau.")
-                continue
-        else:
-            colonne = minimax(copy.deepcopy(plateau), "O", 8, -numpy.inf, numpy.inf)[1]
-            # while not coup_valide(plateau, colonne):
-            #     colonne = random.randint(0, nb_colonnes - 1)
-            stocker_piece(plateau, colonne, piece_actuelle)
-            print(f"L'ordinateur a choisi la colonne {colonne}.")
-
-        if est_gagnant(plateau, piece_actuelle, [obtenir_ligne(plateau, colonne) + 1, colonne]):
-            afficher_plateau(plateau)
-            if piece_actuelle == 'X':
-                print(f"Joueur {piece_actuelle} a gagné !")
-            else:
-                print("L'ordinateur a gagné !")
-            break
-        elif plateau_plein(plateau):
-            afficher_plateau(plateau)
-            print("Match nul !")
-            break
-        else:
-            piece_actuelle = 'O' if piece_actuelle == 'X' else 'X'
 
 def minimax(plateau_simule, piece, profondeur, alpha, beta):
     if est_gagnant_minimax(plateau_simule, "O"):
@@ -279,6 +160,3 @@ def minimax(plateau_simule, piece, profondeur, alpha, beta):
                 if beta <= alpha:
                     break
         return pire_score, pire_colonne
-
-if __name__ == "__main__":
-    jouer()
